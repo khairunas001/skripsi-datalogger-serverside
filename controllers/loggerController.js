4152d8fb01ea6171b601194d39c207f0a3745016
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-08-03 23:05:00 +0700

    server v3

--- controllers/loggerController.js
+++ controllers/loggerController.js
@@ -1,53 +1,52 @@
-const {logger} = require('../models');
-//lakukan path sampai models saja yg di dimpoort file loggernya
+const { logger } = require('../models');
 
 class LoggerController {
-    static getLogger(req, res) {
-      logger.findAll({
-        order: [['id', 'DESC']]
-      })
-      .then(loggers => {
-        res.json(loggers);
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-      });
-    }
-  
-    static add(req, res) {
-      const { tanggal, waktu, suhu } = req.body;
-  
-      logger.create({
-        tanggal, waktu, suhu
-      })
-      .then(result => {
-        res.json(result);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-    }
-  
-    static deleteLogger(req, res) {
-      const { id } = req.params;
-  
-      logger.destroy({
-        where: { id }
-      })
-      .then(deleted => {
-        if (deleted) {
-          res.json({ message: 'Data deleted successfully' });
-        } else {
-          res.status(404).json({ message: 'Data not found' });
-        }
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-      });
-    }
+  static getLogger(req, res) {
+    logger.findAll({
+      order: [['id', 'DESC']],
+      limit: 30
+    })
+    .then(loggers => {
+      res.json(loggers);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
   }
-  
+
+  static add(req, res) {
+    const { tanggal, waktu, suhu } = req.body;
+
+    logger.create({
+      tanggal, waktu, suhu
+    })
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  }
+
+  static deleteLogger(req, res) {
+    const { id } = req.params;
+
+    logger.destroy({
+      where: { id }
+    })
+    .then(deleted => {
+      if (deleted) {
+        res.json({ message: 'Data deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Data not found' });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+  }
+}
 
 module.exports = LoggerController;
